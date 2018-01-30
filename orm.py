ea3379d2de5711dbef94e639cc510d519917b9b4
johnlofty <johnlofty@163.com>
2018-01-30 11:47:29 +0800

    fixbugs;

--- orm.py
+++ orm.py
@@ -1,67 +1,5 @@
-
-
-@asyncio.coroutine
-
-def creat_pool(loop,**kw):
-	logging.info('creat datebase connection pool...')
-	global__pool
-	__pool = yield from aiomysql.creat_pool(
-		host = kw.get('host',localhost),
-		port = kw.get('port',3306),
-		user = kw['user'],
-		password = kw['password'],
-		db = kw['db'],
-		charset = kw.get('charset','utf8'),
-		autocommit = kw.get('autocommit',True),
-		maxsize = kw.get('maxsize',10),
-		minsize = kw.get('minsize',1),
-		loop = loop
-	)
-
-@asyncio.coroutine
-
-def select(sql,args,size = None):
-	log(sql,args)
-	global __pool
-	with(yield from __pool) as conn:
-		cur = yield from conn.cursor(aiomysql.DictCursor)
-		yield from cur.execute(sql.replace('?',"%s"),args or ())
-		if size:
-			rs = yield from cur.fetchmany(size)
-		else:
-			rs = yield from cur.fetchall()
-		yield from cur.close()
-		logging.info('rows returned:%s' % len(rs))
-		return rs
-
-
-@asyncio.coroutine
-
-def execute(sql, args):
-	log(sql)
-	with (yield from __pool) as conn:
-		try:
-			cur = yield from comm.cursor()
-			yield from cur.execute(sql.replace('?' , '%s') , args)
-			affected = cur.rowcount
-			yield from cur.close()
-		except BaseException as e:
-			raise
-		return affecyed
-
-
-from orm import Model,StringField,IntegerField
-
-class User(Model):
-	__table__ = 'users'
-
-	id = IntegerField(primary_key = True)
-	name = StringField()
-
-
-#定义Modle
-
-class Modle(dict,metaclass = ModelMet):
+#定义Model
+class Model(dict,metaclass = ModelMet):
 
 	def __init__(self,**kw):
 		super(Model,self).__init__(**kw)
@@ -80,7 +18,7 @@ class Modle(dict,metaclass = ModelMet):
 		return getattr(self,key,None)
 
 	def getValueOrDefault(self,key):
-		value = getattr(self,key,None)	
+		value = getattr(self,key,None)
 		if value is None:
 			field = self.__mappings__[key]
 			if field.default is not None:
@@ -177,5 +115,3 @@ class Model(dict):
 		rows = yield from execute(self.__insert__,args)
 		if rows != 1:
 			logging.warn('failed to insert record: affected rows:%s' % rows)
-
-
